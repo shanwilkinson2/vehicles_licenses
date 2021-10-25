f37845db3a9facf10d5a25eb541590614fc2560c
Shhh <shanwilkinson2@users.noreply.github.com>
2021-10-25 19:16:39 +0100

    download files

--- get_data.R
+++ get_data.R
@@ -2,6 +2,8 @@
 
 library(XML)
 library(dplyr)
+library(readxl)
+
 
 # driving license holders
 # https://data.gov.uk/dataset/d0be1ed2-9907-4ec4-b552-c048f6aec16a/gb-driving-licence-data
@@ -15,3 +17,6 @@ free(doc)
 links2 <- data.frame(link = links) %>%
   filter(grepl("https://data.dft.gov.uk/driving-licence-data/", link))
 
+# need to download files first then read
+download.file(links2$link[2], 
+              destfile = paste0(getwd(), stringr::str_sub(links2$link[2], 45, -1) ))
